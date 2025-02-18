f1e1a5373c07d28de03618ced78de160353dbcef
holeyko <mr.holeyko@gmail.com>
2025-02-18 14:48:52 +0300

    update scripts

---
+++ run.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+    echo "Error: No argument provided."
+    echo "Usage: $0 {run|build|rab}"
+    exit 1
+fi
+
+check_env() {
+    if [ -z "${TELEGRAM_BOT_TOKEN}" ]; then
+        echo "TELEGRAM_BOT_TOKEN is not set."
+        exit 1
+    fi
+}
+
+run() {
+    docker run --rm -d \
+        -e TELEGRAM_BOT_TOKEN=$TELEGRAM_BOT_TOKEN \
+        --name inanny-bot \
+        inanny-bot
+}
+
+build() {
+    docker build -t inanny-bot .
+}
+
+case "$1" in
+    run)
+        echo "Running Docker container..."
+        check_env
+        run
+        ;;
+    build)
+        echo "Building Docker image..."
+        build
+        ;;
+    rab)
+        echo "Rebuilding and running Docker container..."
+        check_env
+        build
+        run
+        ;;
+    *)
+        echo "Error: Invalid argument."
+        echo "Usage: $0 {run|build|rab}"
+        exit 1
+        ;;
+esac
--- scripts/run.sh
+++
@@ -1,4 +0,0 @@
-docker run --rm -d \
-    -e TELEGRAM_BOT_TOKEN=$TELEGRAM_BOT_TOKEN \
-    --name inanny-bot \
-    inanny-bot
