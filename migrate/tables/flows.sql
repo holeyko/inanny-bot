da3251eb4442d78bbaffbbf56f8294bca18a0304
holeyko <mr.holeyko@gmail.com>
2025-02-23 17:06:38 +0300

    fix sql migration

--- migrate/tables/flows.sql
+++ migrate/tables/flows.sql
@@ -2,8 +2,9 @@
 
 -- changeset holeko:create_flows
 CREATE TABLE IF NOT EXISTS flows(
-    tg_id BIGINT,
-    name VARCHAR(255) NOT NULL,
+    tg_id BIGINT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
     context JSONB NOT NULL,
-    PRIMARY KEY(tg_id, name)
+
+    PRIMARY KEY (tg_id, "name")
 );
