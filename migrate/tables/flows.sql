c300fa18aed33ec4397a88c53c4f124c9ad5ff3c
holeyko <mr.holeyko@gmail.com>
2025-03-23 10:17:21 +0300

    Change column name

--- migrate/tables/flows.sql
+++ migrate/tables/flows.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS flows(
     tg_id BIGINT NOT NULL,
     "name" VARCHAR(255) NOT NULL,
     step VARCHAR(255) NOT NULL,
-    context JSONB NOT NULL,
+    "data" JSONB NOT NULL,
 
     PRIMARY KEY (tg_id, "name")
 );
