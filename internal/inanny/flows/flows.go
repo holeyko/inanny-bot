698a6b76f335e96a0e7821daba277ac1e587f910
holeyko <mr.holeyko@gmail.com>
2025-03-23 10:19:25 +0300

    Fix package name

--- internal/inanny/flows/flows.go
+++ internal/inanny/flows/flows.go
@@ -1,7 +1,23 @@
 package flows
 
+type FLowName string
+
 type FlowStep string
 
-type Flow[CONTEXT any] interface {
-	HandleStep(curStep FlowStep, context CONTEXT) (nextStep FlowStep, err error)
+type Flow[CONTEXT any, DATA any] interface {
+	HandleStep(curStep FlowStep, context CONTEXT, DATA data) (nextStep FlowStep, err error)
+}
+
+type FlowState[DATA any] struct {
+	flowName FLowName
+	step     FlowStep
+	data 	 DATA
+}
+
+const (
+	AddReqularPoll FLowName = "AddReqularPoll"
+)
+
+type CommonStep struct {
+	Initial FlowStep = "Initial"
 }
